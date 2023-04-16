06406defc123b55a2ad76b038dc0dc19bcde6176
chenjh <>
2023-04-16 20:55:19 +0800

    dataIdArgCount

--- task-tool/src/main/java/cn/org/opendfl/tasktool/task/TaskComputeVo.java
+++ task-tool/src/main/java/cn/org/opendfl/tasktool/task/TaskComputeVo.java
@@ -25,6 +25,12 @@ public class TaskComputeVo {
      * 仅分类，没有其他作用
      */
     private String category;
+    /**
+     * 数据id的参数序号，-1表示无参
+     * 0表示第一个参数，如果无参数的话也兼容
+     * 建议放第1个，以免在中间增加参数时序号变了
+     */
+    private int dataIdArgCount;
 
     /**
      * 用于显示正在执行的数据
